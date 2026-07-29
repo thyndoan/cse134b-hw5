d71a08e2167107670e1de93a704524793e14e858
Thy Doan <tndoan@ucsd.edu>
2026-07-29 16:40:43 -0700

    add localStorage

--- js/index.js
+++ js/index.js
@@ -3,7 +3,16 @@ const themeInputs = document.querySelectorAll('input[name="theme"]');
 
 root.classList.add("js-enabled");
 
+/*Apply localStorage for theme persists across page loads and across pages */
+let current;
+try {
+  current = localStorage.getItem("theme") || "auto";
+} catch (e) {
+  current = "auto";
+}
+
 themeInputs.forEach((input) => {
+  input.checked = input.value === current;
   input.addEventListener("change", (event) => {
     const choice = event.target.value;
     if (choice === "auto") {
@@ -11,5 +20,8 @@ themeInputs.forEach((input) => {
     } else {
       root.setAttribute("data-theme", choice);
     }
+    try {
+      localStorage.setItem("theme", choice);
+    } catch (e) {}
   });
 });
