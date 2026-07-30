a73db4e29ded9033bda16ecfc0d8a81fe05b79c5
Thy Doan <tndoan@ucsd.edu>
2026-07-29 22:31:22 -0700

    reconstruct other pages for Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/styles");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/imgs");
+  eleventyConfig.addPassthroughCopy("src/files");
 
   return {
     dir: {
