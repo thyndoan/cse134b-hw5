4a0074bd96fe2cfd4deb50d87bfdb4930a469b70
Thy Doan <tndoan@ucsd.edu>
2026-07-30 19:57:39 -0700

    modify config file

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/styles");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/imgs");
+  eleventyConfig.addPassthroughCopy("src/files");
+
   return {
     dir: {
       input: "src",
       output: "_site",
+      includes: "_includes",
+      data: "_data",
     },
   };
 };
