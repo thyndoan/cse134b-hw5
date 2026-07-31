300962eddcd7629baa64f94750884222571a2568
Thy Doan <tndoan@ucsd.edu>
2026-07-30 19:01:07 -0700

    add part3 with 3 project sites generated and a generated 404 site

---
+++ src/_data/site.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: "Thy Doan - Portfolio",
+  description: "Thy Doan's portfolio website",
+  author: "Thy Doan",
+  year: new Date().getFullYear(),
+  nav: [
+    { text: "About", url: "/about/" },
+    { text: "Resume", url: "/resume/" },
+    { text: "Projects", url: "/projects/" },
+    { text: "Contact", url: "/contact/" },
+    { text: "Cat Images", url: "/cat-img-generator/" },
+  ],
+};
